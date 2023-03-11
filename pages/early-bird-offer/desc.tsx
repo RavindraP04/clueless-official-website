0fe1eb43a328fcb585aa0ec6050d44f871674aae
Mukesh Kuiry <99157367+bbsdevloper@users.noreply.github.com>
2023-03-11 17:21:06 +0530

    Ealy bird offer page created

---
+++ pages/early-bird-offer/desc.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Description = () => {
+  return (
+    <div className='text-white'>
+      This is Description
+    </div>
+  )
+}
+
+export default Description
---
+++ pages/early-bird-offer/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Description from './desc'
+
+const EarlyBird = () => {
+  return (
+    <div>
+      This is early bird page
+      This is early bird page
+      This is early bird page
+      This is early bird page
+      This is early bird page
+      This is early bird page
+      This is early bird page
+      This is early bird page
+      This is early bird page
+      This is early bird page
+      This is early bird page
+      This is early bird page
+      This is early bird page
+      This is early bird page
+      <Description/>
+    </div>
+  )
+}
+
+export default EarlyBird
